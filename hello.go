436ff43edddc22370b1d75e4e6aff79175c84440
Miguel Alcantara <miguel.alc@gmail.com>
2021-05-21 17:16:13 -0500

    adding reverse fn

--- hello.go
+++ hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/ekimike/go_module_hello/morestrings"
+) 
 
 func main() {
-	fmt.Println("hola mundo")
+	fmt.Println(morestrings.ReverseRunes("hola mundo"))
 }
